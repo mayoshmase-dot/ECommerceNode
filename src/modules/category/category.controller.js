09bba2198af0f9260dbbd5b1c8d604f381056204
mayoshmase-dot <mayoshmase@gmail.com>
2026-09-07 02:15:41 +0300

    update & details category

--- src/modules/category/category.controller.js
+++ src/modules/category/category.controller.js
@@ -1,28 +1,50 @@
 import slugify from 'slugify'
 import categoryModel from '../../../DB/models/category.model.js'
 
-export const create = async (req,res)=>{
-const {name} = req.body
-req.body.slug = slugify(name)
-req.body.createdBy = req.id
-req.body.updatedBy = req.id
-const category = await categoryModel.create(req.body)
-return res.status(200).json({message:"success", category})
+export const create = async (req, res) => {
+    const { name } = req.body
+    req.body.slug = slugify(name)
+    req.body.createdBy = req.id
+    req.body.updatedBy = req.id
+    const category = await categoryModel.create(req.body)
+    return res.status(200).json({ message: "success", category })
 }
-export const get = async (req,res)=>{
+export const get = async (req, res) => {
     const categories = await categoryModel.find({})
-return res.status(200).json({message:"success", categories})
+    return res.status(200).json({ message: "success", categories })
 }
-export const getActive = async (req,res)=>{
-    const categories = await categoryModel.find({status:'active'})
-return res.status(200).json({message:"success", categories})
+export const getActive = async (req, res) => {
+    const categories = await categoryModel.find({ status: 'active' })
+    return res.status(200).json({ message: "success", categories })
 }
-export const remove = async (req,res)=>{
+export const details = async (req, res) => {
     const {id} = req.params
-    const categories = await categoryModel.findByIdAndDelete(id)
-if(!categories){
- return res.status(404).json({message:"category not found"})
+    const categories = await categoryModel.findById(id)
+    if (!categories) {
+        return res.status(404).json({ message: "categories not found" })
+    }
+    return res.status(200).json({ message: "success", categories })
 }
-return res.status(200).json({message:"success"})
-
+export const update = async (req, res) => {
+    const { id } = req.params
+    const { name } = req.body
+    const userId = req.id
+    const category = await categoryModel.findById(id)
+    if (!category) {
+        return res.status(404).json({ message: "category not found" })
+    }
+    category.name = name
+    category.updatedBy = userId
+    category.slug = slugify(name)
+    category.status = req.body.status
+    await category.save()
+    return res.status(200).json({ message: "success" })
+}
+export const remove = async (req, res) => {
+    const { id } = req.params
+    const categories = await categoryModel.findByIdAndDelete(id)
+    if (!categories) {
+        return res.status(404).json({ message: "category not found" })
+    }
+    return res.status(200).json({ message: "success" })
 }
--- src/modules/category/category.router.js
+++ src/modules/category/category.router.js
@@ -6,6 +6,8 @@ const router = Router()
 router.post('/',auth(['admin']),controller.create)
 router.get('/',controller.get)
 router.get('/active',controller.getActive)
+router.get('/:id',controller.details)
+router.put('/:id',controller.update)
 router.delete('/:id',controller.remove)
 
 export default router
