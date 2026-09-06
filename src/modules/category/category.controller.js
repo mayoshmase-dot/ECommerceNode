723771670b103e798c50bd16eb50faeb8bf63ba9
mayoshmase-dot <mayoshmase@gmail.com>
2026-09-07 01:58:11 +0300

    delete category

--- src/modules/category/category.controller.js
+++ src/modules/category/category.controller.js
@@ -17,3 +17,12 @@ export const getActive = async (req,res)=>{
     const categories = await categoryModel.find({status:'active'})
 return res.status(200).json({message:"success", categories})
 }
+export const remove = async (req,res)=>{
+    const {id} = req.params
+    const categories = await categoryModel.findByIdAndDelete(id)
+if(!categories){
+ return res.status(404).json({message:"category not found"})
+}
+return res.status(200).json({message:"success"})
+
+}
--- src/modules/category/category.router.js
+++ src/modules/category/category.router.js
@@ -6,5 +6,6 @@ const router = Router()
 router.post('/',auth(['admin']),controller.create)
 router.get('/',controller.get)
 router.get('/active',controller.getActive)
+router.delete('/:id',controller.remove)
 
 export default router
